2da79b62c538d89d6f1f988736db202d76620731
Yogsya <111467261+Yogsya@users.noreply.github.com>
2022-09-16 21:24:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 global.owner = [
-  ['6285346545126', 'Acel', true],
+  ['6283822892316', 'Acel', true],
   // [number, dia creator/owner?, dia developer?]
   JSON.parse(fs.readFileSync('./src/owner.json'))
 ] // Put your number here
@@ -45,8 +45,8 @@ if (symbol.symbol == '') {
 } 
 global.packname = sticker_name 
 global.author = sticker_author
-global.wm = hias_watermark
-global.sYm = hias_symbol
+global.wm = KudoHei
+global.sYm = 
 let list = `Angka
 Asahotak
 Caklontong 
